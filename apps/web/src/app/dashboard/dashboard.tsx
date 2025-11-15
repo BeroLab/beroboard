89a9f046e092cb2c012ded8acbba9cda7c3e1e06
Vitor <vitorhugosnp@gmail.com>
2025-11-14 23:34:51 -0300

    chore: format frontend files

--- apps/web/src/app/dashboard/dashboard.tsx
+++ apps/web/src/app/dashboard/dashboard.tsx
@@ -1,10 +1,6 @@
 "use client";
 import type { authClient } from "@/lib/auth-client";
 
-export default function Dashboard({
-	session,
-}: {
-	session: typeof authClient.$Infer.Session;
-}) {
-	return <>{session}</>;
+export default function Dashboard({ session }: { session: typeof authClient.$Infer.Session }) {
+   return <>{session}</>;
 }
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,37 +5,35 @@ import Header from "@/components/header";
 import Providers from "@/providers/providers";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+   variable: "--font-geist-sans",
+   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+   variable: "--font-geist-mono",
+   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "beroboard",
-	description: "beroboard",
+   title: "beroboard",
+   description: "beroboard",
 };
 
 export default function RootLayout({
-	children,
+   children,
 }: Readonly<{
-	children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<Providers>
-					<div className="grid h-svh grid-rows-[auto_1fr]">
-						<Header />
-						{children}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+   return (
+      <html lang="en" suppressHydrationWarning>
+         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <Providers>
+               <div className="grid h-svh grid-rows-[auto_1fr]">
+                  <Header />
+                  {children}
+               </div>
+            </Providers>
+         </body>
+      </html>
+   );
 }
