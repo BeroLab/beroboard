762594ea3a60fcb8ad788f7cfc644ccd66fd8049
gabriel.pereira1788@gmail.com <gabriel.andrade@ploomes.com>
2025-11-17 20:19:27 -0300

    feat: add db types

--- apps/web/src/modules/projects/services/get-projects/useGetProjects.ts
+++ apps/web/src/modules/projects/services/get-projects/useGetProjects.ts
@@ -1,3 +1,26 @@
 import { apiClient } from "@/lib/client";
+import { useQuery } from "@tanstack/react-query";
+import { ProjectsQueryKeys } from "../../types";
+import type { UserModel,ProjectsModel } from "@beroboard/db/types";
 
-export function useGetProjects() {}
+async function getProjectsService() {
+   const { data } = await apiClient.projects.get({
+      query: {
+         limit: 10,
+      },
+   });
+
+   return data;
+}
+
+export async function useGetProjects() {
+   const { data, isPending } = useQuery({
+      queryKey: [ProjectsQueryKeys.GET_PROJECTS],
+      queryFn: getProjectsService,
+   });
+
+   return {
+      projects: data,
+      isPending,
+   };
+}
---
+++ packages/db/src/types.ts
@@ -0,0 +1,3 @@
+export * from "../prisma/generated/models";
+export * from "../prisma/generated/enums";
+export * from "../prisma/generated/commonInputTypes";
