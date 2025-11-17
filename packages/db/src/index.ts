c1b44527058c657b67f85f6b5b008e6c4ee3add2
Vitor <vitorhugosnp@gmail.com>
2025-11-17 13:36:12 -0300

    fix: prisma .env path

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "bun";
+import dotenv from "dotenv";
 import { PrismaClient } from "../prisma/generated/client";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
+   path: path.join(__dirname, "../../../apps/server/.env"),
+});
 
 const prisma = new PrismaClient();
 
